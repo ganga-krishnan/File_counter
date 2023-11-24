0e5f0be684b893ab20f1e3bba6a2edf6c2d9fd97
ganga-krishnan <95574166+ganga-krishnan@users.noreply.github.com>
2023-11-24 11:35:17 +0530

    Update Directory_file_counter.py

--- Directory_file_counter.py
+++ Directory_file_counter.py
@@ -1,16 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # # Directory File Counter
 
-# In[1]:
-
-
 import os
-
-
-# In[6]:
-
 
 def count_files_with_extension(directory, ext):
     if not os.path.exists(directory):
@@ -31,4 +21,3 @@ directory_path = input("Enter directory path: ")
 file_extension = input("Enter file extension (e.g., '.txt'): ")
 
 count_files_with_extension(directory_path, file_extension)
-
